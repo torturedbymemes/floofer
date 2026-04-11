d88cc3abbd27c76450d29c924925f1ff96967962
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-04-11 13:56:02 +0300

    Unmark SpawnAndDeleteAllEntitiesOnDifferentMaps as explicit

--- Content.IntegrationTests/Tests/EntityTest.cs
+++ Content.IntegrationTests/Tests/EntityTest.cs
@@ -21,7 +21,6 @@ namespace Content.IntegrationTests.Tests
         private static readonly ProtoId<EntityCategoryPrototype> SpawnerCategory = "Spawner";
 
         [Test]
-        [Explicit] // Floofstation - OOM bait
         public async Task SpawnAndDeleteAllEntitiesOnDifferentMaps()
         {
             // This test dirties the pair as it simply deletes ALL entities when done. Overhead of restarting the round
